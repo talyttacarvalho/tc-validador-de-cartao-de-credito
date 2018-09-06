4e6f726e6b945b4e504c0bfcacbfcd840f572d99
Talytta Carvalho <talytta110@hotmail.com>
2018-09-05 23:29:11 -0300

    att tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,37 +2,37 @@ const assert = require("assert");
 const chai = require("chai");
 const expect = chai.expect;
 
-const cardValidator = require("../index.js");
+const lib = require("../index.js");
 
 describe("cardValidator()", function () {
   describe("Validar cartão com dígitos corretos.", function () {
     it("Deve retornar true", () => {
-      assert.equal(cardValidator("36490102462661"), true);
+      assert.equal(lib.cardValidator("36490102462661"), true);
     });
   });
 
   describe("Validar cartão com dígito incorreto.", function () {
     it("Deve retornar false", () => {
-      assert.equal(cardValidator("36490102462669"), false);
+      assert.equal(lib.cardValidator("36490102462669"), false);
     });
   });
 
   describe("Validar número incompleto.", function () {
     it("Deve retornar false.", () => {
-      assert.equal(cardValidator("3649010"), false);
+      assert.equal(lib.cardValidator("3649010"), false);
     });
   });
 
   describe("Quando não hover parâmetro.", function () {
     it('Deve retornar erro "Número do cartão não inserido."', () => {
-      let emptyPar = () => {cardValidator("")};
+      let emptyPar = () => {lib.cardValidator("")};
       expect(emptyPar).to.throw("Número do cartão não inserido.");
     });
   });
 
   describe("Validar letras inseridas no campo.", function () {
     it('Deve retornar "Cartão com dígitos inválidos (letras)."', () => {
-      let errorPar = () => {cardValidator("36A9O1OZA6Z669")};
+      let errorPar = () => {lib.cardValidator("36A9O1OZA6Z669")};
       expect(errorPar).to.throw("Cartão com dígitos inválidos (letras).");
     });
   });
